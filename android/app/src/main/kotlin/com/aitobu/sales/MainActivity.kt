180a47dadb43ca961b94ee7b8e9bd902345b186f
Akid Fikri Azhar <akidfikriazhar@gmail.com>
2024-12-13 15:10:24 +0800

    set branding icon, name and splash

---
+++ android/app/src/main/kotlin/com/aitobu/sales/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aitobu.sales
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Aitobu Sales',
+      title: 'Aitobu POS',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
